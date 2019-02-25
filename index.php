ca168292a1238e4f804ce636b19bb9af923bfb71
abdelhameeed <45479541+abdelhameeed@users.noreply.github.com>
2019-02-25 13:06:31 +0200

    Add files via upload
    comments handling.

--- index.php
+++ index.php
@@ -25,7 +25,28 @@
      			$contjson=json_decode($cont);
      			print_r($contjson);
 			}
+			//this for send the value to youtube api
+			$name = str_replace(" ", "%20", $contjson->text) ;
+    		$youtube_url ="https://www.googleapis.com/youtube/v3/search?key=AIzaSyA_76L_psuS5PJ-LMBUMpoXiYl7Or12GAo&q=".$name."&part=snippet&type=video";
+     		$cont=file_get_contents($youtube_url)     ;
+     		$contjson=json_decode($cont);
+     		if(count($contjson->items)==0)
+     		{
+     			echo "There is no video to show we are sorry";
+     		}else{
+				 $value= $contjson->items[0]->id->videoId;
+				 $_SESSION['vid']=$value;
+				 
+     			$chanelid=$contjson->items[0]->snippet->channelId;
+     			//view video comment
 
+     			$view="https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&key=AIzaSyA_76L_psuS5PJ-LMBUMpoXiYl7Or12GAo&videoId=".$value;
+
+     			$view=file_get_contents($view);
+				 $view=json_decode($view);
+				 $_SESSION['view']=$view;
+
+     		}
 		}
 	}
 ?>
