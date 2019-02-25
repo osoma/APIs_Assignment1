8fdb06a53485a5b01a50c7518f1e0305c8b3199a
abdelhameeed <45479541+abdelhameeed@users.noreply.github.com>
2019-02-25 12:59:34 +0200

    Add files via upload
    A fisrt API which get number facts or a year

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
 <?php
+	session_start();
+	if(isset($_GET['number']))
+	{
+		if(!empty($_GET['number']))
+		{	//get the fact for a number or a year
+			if($_GET["value"]=="1")
+			{
+				$number_url ="http://numbersapi.com/".$_GET['number']."/math?json";
+     			$cont=file_get_contents($number_url);
+     			$contjson=json_decode($cont);
+     			print_r($contjson);
+			}
+			if($_GET["value"]=="2")
+			{
+				$number_url ="http://numbersapi.com/".$_GET['number']."/year?json";
+     			$cont=file_get_contents($number_url);
+     			$contjson=json_decode($cont);
+     			print_r($contjson);
+			}
+			
+			//this for send the value to youtube api
+			
+		}
+	}
 ?>
