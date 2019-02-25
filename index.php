18404b9c2013fb1876c32a031dced2293e115c5a
osoma <34522776+osoma@users.noreply.github.com>
2019-02-25 12:32:24 +0200

    Add files via upload
    this form is to ask user to enter a animal or number or year

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
-<?php
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="" method="GET">
+		<input type="text" name="number" placeholder="enter a Number ,a Year Or an animal">
+		<br>
+		<select name="value">
+			<option value="1">number</option>
+			<option value="2">Year</option>
+			<option value ="3">animal</option>
+		</select>
+		<input type="submit" value="search">
+	</form><br>
+</body>
+</html>
